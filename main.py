677bb47c54e3d6349d1b28d083763708374cb2b0
Diego Silva <odiegosilvadev@gmail.com>
2026-03-25 14:53:20 -0300

    add: main.py

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-from database import db, Usuario, Anuncio
 
-db.connect()
-db.create_tables([Usuario, Anuncio])
 
-# Criando um usuário
-#usuario1 = Usuario.create(nome='João Silva', email='joao.silva@example.com', senha='senha123')
-#usuario1 = Usuario.create(nome='Maria Silva', email='maria.silva@example.com', senha='senha123')
-#usuario1 = Usuario.create(nome='jujubinha', email='jujubinha@example.com', senha='senha123')
 
-lista_usuarios = Usuario.select()
-print("Lista de usuários:")
 
-for u in lista_usuarios:
-    print("-", u.nome, "-", u.email)
+
